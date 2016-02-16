36204d9824a65de0230052c56dc0c392940506c2
Hu Jun <hujun123qwe@163.com>
2016-02-16 12:12:43 +0800

    bootstrap:

--- index.php
+++ index.php
@@ -3,28 +3,30 @@
 <title>Keep accounts</title>
 <meta charset="utf-8">
 <meta lang="zh-cn">
+<link type="text/css" rel="stylesheet" href="css/ui.css">
 </head>
 <body>
-<div style="margin: 0 auto; width: 80%">
-<div style="float:left; width: 49.5%; min-height: 580px; border: 1px solid green">
+<div class="div_all">
+<div class="div_left">
 <?php  
 	echo "This website is built for keeping accounts.";
 ?>
-<form>
-<ul style="text-decoration: none; d">
-	<li style="text-decoration: none;">
-		<input type="text" value="时间">
-	</li>
-	<li>
-		<input type="text" value="消费类型">
-	</li>
-	<li>
-		<input type="text" value="消费金额">
-	</li>
-</ul>
+<form method="post" action="script.cgi">
+  <ul>
+    <li><label for=name>Name:</label>
+        <input id=name name=name></li>
+    <li><label for=email>Email:</label>
+        <input id=email name=email type=email></li>
+    <li><label for=tel>Telephone:</label> 
+        <input id=tel name=tel type=tel></li>
+    <li><label for=url>Homepage:</label>
+        <input id=url name=url type=url></li>
+    <li><label for=bday>Birthday:</label>
+        <input id=bday name=bday type=date></li>
+  </ul>
 </form>
 </div>
-<div style="float:right;width: 49.5%; min-height: 580px; border: 1px solid grey">
+<div class="div_right">
 </div>
 </div>
 </body>
