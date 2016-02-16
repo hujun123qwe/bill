f0b819928196cfbbb0db1bb8f6632bd9ebb13faf
Hu Jun <hujun123qwe@163.com>
2016-02-16 11:31:52 +0800

    init

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Keep accounts</title>
+<meta charset="utf-8">
+<meta lang="zh-cn">
+</head>
+<body>
+<div style="margin: 0 auto; width: 80%">
+<div style="float:left; width: 49.5%; min-height: 580px; border: 1px solid green">
+<?php  
+	echo "This website is built for keeping accounts.";
+?>
+<form>
+<ul style="text-decoration: none; d">
+	<li style="text-decoration: none;">
+		<input type="text" value="时间">
+	</li>
+	<li>
+		<input type="text" value="消费类型">
+	</li>
+	<li>
+		<input type="text" value="消费金额">
+	</li>
+</ul>
+</form>
+</div>
+<div style="float:right;width: 49.5%; min-height: 580px; border: 1px solid grey">
+</div>
+</div>
+</body>
+</html>
